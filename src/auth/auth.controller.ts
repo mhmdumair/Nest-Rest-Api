b0e00cba312de641d9d3fa5344e0b9522dc2d6dc
Umair <umairzahiraj16@gmail.com>
2025-03-14 21:24:40 +0530

    Basic class(for interface) sytem is builded. Validation pipe is used to validate req.body

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { Request } from "express";
+import { AuthDto } from "./dto";
 
 @Controller("auth")
 export class AuthController{
     constructor(private authService:AuthService){}
+
+    
+    // signup(@Req() req:Request){
+    //     console.log(req);
+
+    @Post("signup")
+    signup(@Body() body:AuthDto){
+        console.log(body);
+        return this.authService.signup()
+    }
 
     @Post("signin")
     signin(){
         return this.authService.signin()
     }
 
-    @Post("signup")
-    signup(){
-        return this.authService.signup()
-    }
 }
